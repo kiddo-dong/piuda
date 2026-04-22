d7b7f106247072a27ed7a15c4984d56c3880fc2c
HyunDong <dh655933@gmail.com>
2026-04-22 16:19:47 +0900

    Project init

---
+++ src/main/java/project/piuda/PiudaApplication.java
@@ -0,0 +1,13 @@
+package project.piuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/piuda/PiudaApplicationTests.java
@@ -0,0 +1,13 @@
+package project.piuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
