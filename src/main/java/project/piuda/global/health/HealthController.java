752f8f52d8c49f4969dc4baf8e0e43be9f7d5a36
HyunDong <dh655933@gmail.com>
2026-04-23 20:34:26 +0900

    fix: Health Check API

--- src/main/java/project/piuda/global/health/HealthController.java
+++ src/main/java/project/piuda/global/health/HealthController.java
@@ -1,10 +1,8 @@
 package project.piuda.global.health;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.piuda.global.dto.ApiResponse;
